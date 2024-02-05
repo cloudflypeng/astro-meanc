0326906b671530044503c16a9e7c3936298c7da7
pengyunfei <pengyunfei@360.cn>
2024-02-05 14:00:32 +0800

    init: 迁移博客

---
+++ src/config.ts
@@ -0,0 +1,42 @@
+const authorConfig = {
+  author: 'meanc',
+  avatar: '//avatar.jpg',
+  description: 'A developer who loves to code and learn new things.',
+  email: 'cloudflypeng@gmail.com',
+  github: '',
+  linkedin: '',
+  twitter: '',
+  facebook: '',
+  instagram: '',
+  youtube: '',
+};
+
+const siteConfig = {
+  siteName: 'MEANC-BLOG',
+  description: 'A developer who loves to code and learn new things.',
+  url: 'https://meanc.github.io',
+  lang: 'en',
+  locale: 'en_US',
+  image: '/images/og.jpg',
+  twitter: '@meanc',
+  fbAppId: '',
+  fbPage: '',
+  type: 'website',
+};
+
+const NavMenu = [
+  {
+    name: 'Blog',
+    href: '/posts',
+  },
+  {
+    name: 'About',
+    href: '/about',
+  },
+  {
+    name: 'Connect',
+    href: '/connect',
+  },
+];
+
+export { authorConfig, siteConfig, NavMenu };
---
+++ src/content/posts/webpilot在前端的好用法.md
@@ -0,0 +1,9 @@
+---
+title: webpilot 在前端的用法
+date: 2023-09-27 11:25:49
+tags: design
+---
+
+由于懒得弄图片,可以去juejin看
+
+https://juejin.cn/post/7262542750405754939
